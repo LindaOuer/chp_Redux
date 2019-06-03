0790216282610cfa33e99b5dd13a082faaddaaf2
Linda Ouerfelli <linda.ouerfelli@gmail.com>
2019-06-03 11:03:36 +0100

    Updates

---
+++ precache-manifest.f1cd262f7838a4279ccbc1a492c84233.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b33e29e83d3064ea4960c8261a4e273",
+    "url": "/chp_redux/index.html"
+  },
+  {
+    "revision": "0f83f18ecfa47ed86076",
+    "url": "/chp_redux/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "509e0ec0d906179e12d4",
+    "url": "/chp_redux/static/js/2.20115286.chunk.js"
+  },
+  {
+    "revision": "0f83f18ecfa47ed86076",
+    "url": "/chp_redux/static/js/main.48d254c9.chunk.js"
+  },
+  {
+    "revision": "264787186a6a50363123",
+    "url": "/chp_redux/static/js/runtime~main.9d76a4e9.js"
+  },
+  {
+    "revision": "5f7f13078c28da8d960d8b3347335719",
+    "url": "/chp_redux/static/media/slate_card.5f7f1307.png"
+  }
+]);
